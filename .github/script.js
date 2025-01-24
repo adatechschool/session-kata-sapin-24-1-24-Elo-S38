2246c4f25cee1eeb3d128e1e1912a08411f962a2
Elo-S38 <elodie.sorce@gmail.com>
2025-01-24 12:32:41 +0100

    ajout 1.4 mais etape non validé

--- .github/script.js
+++ .github/script.js
@@ -45,3 +45,42 @@ function afficherTriangleDroite(n) {
 }
 
 afficherTriangleDroite(5);
+
+//1.4 Seconde moitié et espaces
+
+//fonction pour la seconde moitié de la pointe du sapin============> ne fonctionne pas
+let espace = " ";
+function afficherTriangeGauche(n) {
+  for (let i = 0; i < n; i++) {
+    lignEtoile = "";
+
+    for (let etoile = 0; etoile < i; etoile++) {
+      lignEtoile += "*";
+    }
+    lignEtoile += espace;
+    lignEtoile += "\\";
+    console.log(lignEtoile);
+  }
+}
+
+afficherTriangeGauche(5);
+
+//pas eu le temps de tester la methode pour inverser les caracteres string (src=https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/)
+/* function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+}
+
+reverseString("hello"); */
