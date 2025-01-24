d4f05c3db835a468125352e3167b059e66d3445a
Elo-S38 <elodie.sorce@gmail.com>
2025-01-24 11:09:07 +0100

    Ajout 1.2 carré d'étoile + Maj 1.1

--- .github/script.js
+++ .github/script.js
@@ -1,9 +1,10 @@
 //---------------Etape 1-----------------------------------------
-//1.1 Ligne d'étoiles
 
-// afficherEtoiles(n) qui affiche une ligne de * en fonction du paramètre n
+//1.1 Ligne d'étoiles
+let lignEtoile = "";
+
+//fonction afficherEtoiles(n) qui affiche une ligne de * en fonction du paramètre n
 function afficherEtoiles(n) {
-  let lignEtoile = "";
   for (let i = 0; i < n; i++) {
     lignEtoile += "*";
   }
@@ -12,6 +13,19 @@ function afficherEtoiles(n) {
 
 afficherEtoiles(2);
 afficherEtoiles(5);
+
+//1.2 Carré d'étoiles
+
+//fonction afficherRectangle(hauteur, largeur)
+function afficherRectangle(hauteur, largeur) {
+  for (let i = 0; i < hauteur; i++) {
+    lignEtoile = "";
+    afficherEtoiles(largeur);
+  }
+  console.log(lignEtoile);
+}
+
+afficherRectangle(5, 5);
 
 /* //fonction afficherPointeSapin() qui prendra en paramètre la hauteur de ce triangle
 function afficherPointeSapin(hauteur) {}
